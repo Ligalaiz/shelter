4e51fc14b903d9d763a85cf737a35d0452c57f34
Ligalaiz <slavakuz88@gmail.com>
2020-10-26 23:15:33 +0300

    feat: implement slider for main page

--- pages/main/js/main.js
+++ pages/main/js/main.js
@@ -24,3 +24,52 @@ document.addEventListener('keydown', (e) => {
 const smoothScroll = new SmoothScroll(anchor, blockID);
 
 smoothScroll.showSmoothScroll();
+
+// Slider
+const slides = document.querySelectorAll('.slider__item'),
+  prevBtn = document.querySelector('.slider__btn--left'),
+  nextBtn = document.querySelector('.slider__btn--right'),
+  url =
+    'https://raw.githubusercontent.com/rolling-scopes-school/tasks/master/tasks/markups/level-2/shelter/pets.json';
+
+const slider = new Slider(slides);
+
+prevBtn.addEventListener('click', function () {
+  if (slider.isEnabled) {
+    setTimeout(slider.previousItem(slider.slideIndex), 200);
+  }
+});
+
+nextBtn.addEventListener('click', function () {
+  if (slider.isEnabled) {
+    setTimeout(slider.nextItem(slider.slideIndex), 200);
+  }
+});
+
+window.addEventListener('DOMContentLoaded', function () {
+  slider.getData(url).then((data) => {
+    let count = slider.amountCardsOnSlide();
+    slider.initCards(data, count);
+
+    window.addEventListener('resize', () => {
+      count = slider.amountCardsOnSlide();
+      for (let i = 0; i < slides.length; i++) {
+        slides[i].innerHTML = slider.appendCards(data, count);
+      }
+    });
+
+    function appendRandomSlides() {
+      data = data.sort(() => Math.random() - 0.5);
+      document.querySelector(
+        '.slider__item.changeSlides'
+      ).innerHTML = slider.appendCards(data, count);
+    }
+
+    prevBtn.addEventListener('click', function () {
+      appendRandomSlides();
+    });
+    nextBtn.addEventListener('click', function () {
+      appendRandomSlides();
+    });
+  });
+});
